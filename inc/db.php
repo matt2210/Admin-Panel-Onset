afdd8ebc7bd492dbc3be68fa2e65156b4764251a
matt2210 <m2210100@outlook.fr>
2019-12-23 10:46:57 +0100

    Update db.php

--- inc/db.php
+++ inc/db.php
@@ -3,9 +3,9 @@ date_default_timezone_set('Europe/Paris');
 
 try
 {
-  $user = "onset";
-  $pass = "Matt2210!100";
-  $bdd = new PDO('mysql:host=plesk1.dyjix.eu;dbname=gengen', $user, $pass);
+  $user = "LOGIN";
+  $pass = "PASSWORD";
+  $bdd = new PDO('mysql:host=HOST;dbname=DBNAME', $user, $pass);
 }
 catch (Exception $e)
 {
